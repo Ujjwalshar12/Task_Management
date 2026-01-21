8c43fa248026e51068b590573b6f7adb3415a6a6
ujjwalshar12 <ujjwalshar94@gmail.com>
2026-01-21 10:09:54 +0530

    add logger

--- worker/worker.go
+++ worker/worker.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 	"time"
 
+	"task_management/logger"
 	"task_management/repository"
 )
 
@@ -17,9 +18,10 @@ type Worker struct {
 func New(repo *repository.TaskRepository, queue chan string, minutes string) (*Worker, error) {
 	m, err := strconv.Atoi(minutes)
 	if err != nil || m <= 0 {
+		logger.Error("Invalid AUTO_COMPLETE_MINUTES value: %s", minutes)
 		return nil, errors.New("invalid AUTO_COMPLETE_MINUTES")
 	}
-
+	logger.Info("Worker initialized with auto-complete delay=%d minutes", m)
 	return &Worker{
 		Repo:  repo,
 		Queue: queue,
@@ -28,8 +30,11 @@ func New(repo *repository.TaskRepository, queue chan string, minutes string) (*W
 }
 
 func (w *Worker) Start() {
+	logger.Info("Background worker started")
+
 	go func() {
 		for taskID := range w.Queue {
+			logger.Info("Received task %s for auto-completion", taskID)
 			go w.process(taskID)
 		}
 	}()
@@ -40,4 +45,5 @@ func (w *Worker) process(id string) {
 
 	// Only update if still pending/in_progress
 	w.Repo.AutoComplete(id)
+	logger.Info("Task auto-completed successfully: %s", id)
 }
